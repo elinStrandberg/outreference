049b81ff2d4df48c6adae316c30c1a26e199fdfc
Elin Strandberg <elin-strandberg@hotmail.com>
2021-04-03 12:38:26 +0200

    Initial version commit

---
+++ outreference/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace outreference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 2;
+
+            Console.WriteLine(x);
+
+            Double(ref x);
+
+            Console.WriteLine(x);
+        }
+        static void Five (out int a)
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }
+        static void Double (ref int a)
+        {
+            a = a * 2;
+            Console.WriteLine(a);
+        }
+    }
+}
